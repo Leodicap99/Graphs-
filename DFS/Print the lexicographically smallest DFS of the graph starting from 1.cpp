ef1694e4e0a8a507f986187a58572e900b96bdc7
Aditya Skumar <38405597+Leodicap99@users.noreply.github.com>
2020-05-18 19:26:46 +0530

    Update and rename Print the lexicographically smallest DFS of the graph starting from 1.cpp to DFS/Print the lexicographically smallest DFS of the graph starting from 1.cpp

--- DFS/Print the lexicographically smallest DFS of the graph starting from 1.cpp
+++ DFS/Print the lexicographically smallest DFS of the graph starting from 1.cpp
@@ -1,9 +1,10 @@
+/*
 Given a connected graph with N vertices and M edges. The task is to print the lexicographically smallest DFS traversal of the graph starting from 1. 
 Note that the vertices are numbered from 1 to N.
 
 Input: N = 5, M = 8, edges[] = {{1, 4}, {3, 4}, {5, 4}, {3, 2}, {1, 5}, {1, 2}, {1, 3}, {3, 5}}
 Output: 1 2 3 4 5 
-
+*/
 void dfs(vector<int> *adj,vector<bool> &vis,int i)
 {
     vis[i]=true;
