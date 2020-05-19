a5d9b07b84ecd4bef202ec8c54c3abd39f1c691e
Aditya Skumar <38405597+Leodicap99@users.noreply.github.com>
2020-05-19 08:11:26 +0530

    Add files via upload

---
+++ Dijkstra/Dijkstra(variation).cpp
@@ -0,0 +1,46 @@
+/*
+Find all the vertices in the shortest path from source to a destination.
+
+Time Complexity: O(V^2+E)
+*/
+const int INF = 1000000000;
+vector<vector<pair<int, int>>> adj;
+
+void dijkstra(int s, vector<int> & d, vector<int> & p) {
+    int n = adj.size();
+    d.assign(n, INF);
+    p.assign(n, -1);
+    vector<bool> u(n, false);
+
+    d[s] = 0;
+    for (int i = 0; i < n; i++) {
+        int v = -1;
+        for (int j = 0; j < n; j++) {
+            if (!u[j] && (v == -1 || d[j] < d[v]))
+                v = j;
+        }
+
+        if (d[v] == INF)
+            break;
+
+        u[v] = true;
+        for (auto edge : adj[v]) {
+            int to = edge.first;
+            int len = edge.second;
+
+            if (d[v] + len < d[to]) {
+                d[to] = d[v] + len;
+                p[to] = v;
+            }
+        }
+    }
+}
+vector<int> shortest_path(int source,int destination,vector<int> &p)
+{
+	vector<int> path;
+	for(int i=destination;i!=source;i=p[i])
+		path.push_back(i);
+	path.push_back(s);
+	reverse(path.begin(),path.end());
+	return path;
+}
