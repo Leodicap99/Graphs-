6d13bd97cd871aa03cf6408fb9bda0ac73c80de8
Aditya Skumar <38405597+Leodicap99@users.noreply.github.com>
2020-05-19 07:56:53 +0530

    Update Dijkstra Algorithm.cpp

--- Dijkstra/Dijkstra Algorithm.cpp
+++ Dijkstra/Dijkstra Algorithm.cpp
@@ -36,37 +36,35 @@ but no useful work will be done for them. Therefore, the algorithm can be stoppe
 Time Complexity:O(V^2+E)
 
 */
+const int INF = 1000000000;
+vector<vector<pair<int, int>>> adj;
 
-vector<vector<pair<int,int>>> graph;
-const int INF=1e9;
-vector<int> p,d;
+void dijkstra(int s, vector<int> & d, vector<int> & p) {
+    int n = adj.size();
+    d.assign(n, INF);
+    p.assign(n, -1);
+    vector<bool> u(n, false);
 
-void dijkstra()
-{
-	int n=graph.size();
-	d.assign(n,INF);
-	p.assign(n,-1);
-	vector<bool> vis(n,false);
-	for(int i=0;i<n;i++)
-	{
-		int v=-1;
-		for(int j=0;j<n;j++)
-		{
-			if(!vis[j] && (vis==-1 || d[j]<d[v]))
-				v=j;
-		}
-		if(d[v]==INF)
-			break;
-		vis[v]=true;
-		for(auto edge:v)
-		{
-			int to=edge.first;
-			int weight=edge.second;
-			if(d[to]>d[v]+weight)
-			{
-				d[to]=d[v]+weight;
-				p[to]=v;
-			}
-		}
-	}
+    d[s] = 0;
+    for (int i = 0; i < n; i++) {
+        int v = -1;
+        for (int j = 0; j < n; j++) {
+            if (!u[j] && (v == -1 || d[j] < d[v]))
+                v = j;
+        }
+
+        if (d[v] == INF)
+            break;
+
+        u[v] = true;
+        for (auto edge : adj[v]) {
+            int to = edge.first;
+            int len = edge.second;
+
+            if (d[v] + len < d[to]) {
+                d[to] = d[v] + len;
+                p[to] = v;
+            }
+        }
+    }
 }
